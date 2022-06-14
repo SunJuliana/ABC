1b2b4077d2bc879856fdc1750b0fc0172dbb4877
SunJuliana <juliakulisheva@yandex.ru>
2022-06-15 03:45:01 +0700

    learning if - else ending

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,6 @@
 fun main(args: Array<String>) {
     val x = 10
-    val y = 1
-    if (x > y) {
-        println("x is greater than y")
-    } else {
-        println("x is not greater than y")
-    }
+    val y = 16
+    println(if (x > y) "x is greater than y" else "x is not greater than y")
     println("This line runs no matter what")
 }
