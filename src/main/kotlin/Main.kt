6e5ee6c3803506aa31a42f033f2ad6e77ad8fd22
SunJuliana <juliakulisheva@yandex.ru>
2022-06-15 03:39:57 +0700

    learning if - else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-   var x = 1 //Присвоить x значение 1
-   println("Before the loop. x = $x.")
-    while (x<4){
-    println("In the loop.x = $x.")
-    x=x+1
+    val x = 10
+    val y = 1
+    if (x > y) {
+        println("x is greater than y")
+    } else {
+        println("x is not greater than y")
     }
-    println("After the loop. x = $x.")
-
+    println("This line runs no matter what")
 }
